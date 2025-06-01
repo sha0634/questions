71b7ef55d0a99aa2290562ae962fef124dca7bbd
sha0634 <akanksha7343@gmail.com>
2025-06-01 23:57:46 +0530

    Create 2929.cpp

---
+++ 2929.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    long long distributeCandies(int n, int limit) {
+        long long c=0;
+        for(int i=0;i<=min(limit,n);i++){
+            if(n-i>2*limit) continue;
+            c+=min(n-i,limit)-max(0,n-i-limit)+1;
+        }
+        return c;
+    }
+};
